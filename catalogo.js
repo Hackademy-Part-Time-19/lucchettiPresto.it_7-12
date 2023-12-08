d23e353d9c154cc6f47c9972f9a15df72fb95015
GabLux <gabrielelucchetti@live.it>
2023-12-08 10:52:53 +0100

    funzioni java scritte in maniera pulita

--- catalogo.js
+++ catalogo.js
@@ -1,36 +1,90 @@
 let listaImmagini = ["./image/skincare.jpg", "./image/abbigliamento.webp", "./image/telefonia.jpg"]
 let index = 0
 let img = document.getElementById("presentationImage")
-let imgIndex = 0
+let presentationP=[""]
 
-setInterval(() => {
-    if (index == 0) {
 
-        img.src = listaImmagini[index];
-        document.getElementById("presentationImage").style = "opacity:0; display:block";
-        setTimeout(() => { document.getElementById("presentationImage").style = "opacity:1; display:block" }, 1000);
-        setTimeout(() => { document.getElementById("presentationImage").style = "opacity:0; display:block" }, 1500)
 
-    } else if (index == 1) {
 
-        img.src = listaImmagini[index];
-        document.getElementById("presentationImage").style = "opacity:0; display:block";
-        setTimeout(() => { document.getElementById("presentationImage").style = "opacity:1; display:block" }, 1000);
-        setTimeout(() => { document.getElementById("presentationImage").style = "opacity:0; display:block" }, 1500)
+/*
+setInterval(function () {
 
-    } else if (index == 2) {
 
-        img.src = listaImmagini[index];
-        document.getElementById("presentationImage").style = "opacity:0; display:block";
-        setTimeout(() => { document.getElementById("presentationImage").style = "opacity:1; display:block" }, 1000);
-        setTimeout(() => { document.getElementById("presentationImage").style = "opacity:0; display:block" }, 1500)
+    img.src = listaImmagini[index];
+    document.getElementById("presentationImage").style = "opacity:0;display:block ";
+    setTimeout(() => { document.getElementById("presentationImage").style = "opacity:1;display:block " }, 1000);
+    setTimeout(() => { document.getElementById("presentationImage").style = "opacity:0;display:block " }, 3000)
 
-    }
+
 
     index > 1 ? index = 0 : index++;
 
-    console.log(index)
-}, 2500)
+
+}, 3500)*/
 
 
 
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+async function cicloImmagine() {
+
+    img.src = listaImmagini[index++ % listaImmagini.length];
+    await settaStyle([...parametriImmagini])
+
+    
+    cicloImmagine()
+}
+
+let parametriImmagini = [{
+    ritardo: 500,
+    opacita: 0,
+
+},
+{
+    ritardo: 1000,
+    opacita: 0.8
+},
+{
+    ritardo: 2000,
+    opacita: 0
+}]
+
+async function settaStyle( oggetti) {
+    const oggetto = oggetti.shift()
+    if (oggetto == undefined) {
+        return;
+    };
+    img.style = `opacity:${oggetto.opacita}; display:block`;
+    await delay(oggetto.ritardo)
+    return settaStyle(oggetti)
+
+
+}
+
+
+cicloImmagine()
+
+
+
+
+let prodotti=[
+   
+]
+
+function filtraPerPrezzo() {
+
+    let nome="Crema Skincare"
+    let prezzo= "€100,00"
+
+    let OggettiProdototti=[
+        { 
+            nomeProdotto:nome,
+            prezzoProdotto:prezzo,
+            nomeDaFiltrare: nomeProdotto.toLowerCase()
+        }, 
+    ]
+
+    prodotti= prodotti.push(OggettiProdototti)
+}
